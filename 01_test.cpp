a8f7c09d1d1b8f9adf844ab511d2e5802e20c7b4
akhilshekar4991 <as8hu@virginia.edu>
2023-02-13 00:16:24 -0500

    Pushed the initial files

---
+++ 01_test.cpp
@@ -0,0 +1,13 @@
+#include "myHashTable.hpp"
+
+class 
+
+int main()
+{
+    printf("lol.\n");
+    list<int> c1{ 10, 11 };
+
+    c1.push_back(13);
+    print(c1);
+    printf("Length of the list = %d \n", c1.size());
+}
---
+++ myHashTable.hpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+template <typename S> void print(const S& s) {
+    for (const auto& p : s) {
+        cout << "(" << p << ") ";
+    }
+
+    cout << endl;
+}
